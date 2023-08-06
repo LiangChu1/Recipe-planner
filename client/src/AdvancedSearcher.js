c8c7f177fa5f20f475f4405c7d586421ff0fc7a2
LiangChu1 <chuliang686@gmail.com>
2023-08-05 21:48:40 -0400

    added button to show and hide advanced settings

--- client/src/AdvancedSearcher.js
+++ client/src/AdvancedSearcher.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './AdvancedSearcher.css';
 
 function AdvancedSearcher({setSelectedCuisine, setSelectedDiet, setSelectedIntolerances, setSelectedMealTypes}){
@@ -5,12 +6,18 @@ function AdvancedSearcher({setSelectedCuisine, setSelectedDiet, setSelectedIntol
     const dietTypes = ['Gluten Free', 'Ketogenic', 'Vegetarian', 'Vegan', 'Pescetarian', 'Paleo'];
     const intolerances = ['Dairy', 'Egg', 'Gluten', 'Grain', 'Peanut', 'Seafood', 'Sesame', 'Shellfish', 'Soy', 'Sulfite', 'Tree Nut', 'Wheat'];
     const mealTypes = ['main course', 'side dish', 'dessert', 'appetizer', 'salad', 'bread', 'breakfast', 'soup', 'beverage', 'sauce', 'marinade', 'fingerfood', 'snack', 'drink',];
+    const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
+
+    function handleToggleAdvancedOptions() {
+      setShowAdvancedOptions((prevValue) => !prevValue);
+    }
 
     function handleAdvancedInputs(inputType, event){
       setSelectedCuisine('')
       setSelectedDiet('')
       setSelectedIntolerances('')
       setSelectedMealTypes('')
+
         if (inputType === "cuisine") {
           setSelectedCuisine(event.target.value);
         } 
@@ -27,39 +34,48 @@ function AdvancedSearcher({setSelectedCuisine, setSelectedDiet, setSelectedIntol
 
     return (
         <>
-        <label>Cuisine</label>
-        <select onChange={(e) => handleAdvancedInputs("cuisine", e)}>
-          <option value="">Select Cuisine</option>
-          {cuisineTypes.map((type) => {
-            return <option key={type}>{type}</option>
-          })}
-        </select>
+        {showAdvancedOptions && (
+          <>
+            <h4>Advanced Options</h4>
+            <label>Cuisine</label>
+            <select onChange={(e) => handleAdvancedInputs("cuisine", e)}>
+              <option value="">Select Cuisine</option>
+              {cuisineTypes.map((type) => {
+                return <option key={type}>{type}</option>
+              })}
+            </select>
+            <br/>
+            <label>Diet</label>
+            <select onChange={(e) => handleAdvancedInputs("diet", e)}>
+              <option value="">Select Diet</option>
+              {dietTypes.map((type) => {
+                return <option key={type}>{type}</option>
+              })}
+            </select>
+            <br/>
+            <label>Food Intolerances</label>
+            <select onChange={(e) => handleAdvancedInputs("intolerances", e)}>
+              <option value="">Select Food Intolerance</option>
+              {intolerances.map((type) => {
+                return <option key={type}>{type}</option>
+              })}
+            </select>
+            <br/>
+            <label>Meals Types</label>
+            <select onChange={(e) => handleAdvancedInputs("mealType", e)}>
+              <option value="">Select Diet</option>
+              {mealTypes.map((type) => {
+                return <option key={type}>{type}</option>
+              })}
+            </select>
+            <br/>
+          </>
+        )}
+        <button onClick={handleToggleAdvancedOptions}>
+            {showAdvancedOptions ? 'Hide Advanced Options' : 'Show Advanced Options'}
+        </button>
         <br/>
-        <label>Diet</label>
-        <select onChange={(e) => handleAdvancedInputs("diet", e)}>
-          <option value="">Select Diet</option>
-          {dietTypes.map((type) => {
-            return <option key={type}>{type}</option>
-          })}
-        </select>
-        <br/>
-        <label>Food Intolerances</label>
-        <select onChange={(e) => handleAdvancedInputs("intolerances", e)}>
-          <option value="">Select Food Intolerance</option>
-          {intolerances.map((type) => {
-            return <option key={type}>{type}</option>
-          })}
-        </select>
-        <br/>
-        <label>Meals Types</label>
-        <select onChange={(e) => handleAdvancedInputs("mealType", e)}>
-          <option value="">Select Diet</option>
-          {mealTypes.map((type) => {
-            return <option key={type}>{type}</option>
-          })}
-        </select>
-        <br/>
-        </>
+      </>
     )
 }
 
